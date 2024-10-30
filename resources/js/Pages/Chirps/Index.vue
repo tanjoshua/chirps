a8e220e89e329d2420d0ce018413717033ad0f2b
josh <jtanjoshua@gmail.com>
2024-10-30 09:11:32 +0800

    some init

---
+++ resources/js/Pages/Chirps/Index.vue
@@ -0,0 +1,11 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Chirps" />
+    <AuthenticatedLayout>
+        <div>test</div>
+    </AuthenticatedLayout>
+</template>
